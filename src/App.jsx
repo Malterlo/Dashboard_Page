0ba58a4ee2a3946de33486523387e57dddc7997f
Arunratanak <2026376prak@aupp.edu.kh>
2026-09-18 21:22:39 +0700

    added shadcn and installed recharts

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import { BarChart } from "@mui/x-charts/BarChart";
+import Card from "./Components/ui/card.jsx"
+
 
 function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <>
-      <BarChart
-        xAxis={[
-          {
-            id: "barCategories",
-            data: ["bar A", "bar B", "bar C"],
-            height: 28,
-          },
-        ]}
-        series={[
-          {
-            data: [2, 5, 3],
-          },
-        ]}
-        height={300}
-      />
-    </>
-  );
+  return(
+    <Card/>
+  )
 }
 
 export default App;
